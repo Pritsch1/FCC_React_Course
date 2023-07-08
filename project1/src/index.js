f4be3445d464bee38a3f1cefccc11aab2ee67145
Pritsch <pritschdec@gmail.com>
2023-07-08 17:09:07 -0300

    Project 1 update.

--- project1/src/index.js
+++ project1/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './main.js';
+import { Header } from './header.js';
+import { Content } from './content.js';
+import { Footer } from './footer.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+        <Header />
+        <Content />
+        <Footer />
   </React.StrictMode>
 );
--- project1/src/main.js
+++
@@ -1,9 +0,0 @@
-function App() {
-    return (
-    <div>
-    <p>Working!</p>
-    </div>
-    )
-}
-
-export default App;
